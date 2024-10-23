4d8d4c3eb859df89700be4518e35c3229b807af8
Umair <you@example.com>
2024-10-23 09:07:34 +0000

    Dockerfile added

---
+++ HelloWorldApp/src/main/java/com/example/HelloWorldApp.java
@@ -0,0 +1,17 @@
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+public class HelloWorldApp {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello, World!";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+        server.start();
+        System.out.println("Server is listening on port 8080...");
+    }
+}
+
